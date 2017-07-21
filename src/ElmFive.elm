9e94f62e0d512efb8828fab0bf39a5e0b6909a72
Nayan Hajratwala <nayan@chikli.com>
2017-07-21 18:04:27 -0400

    added law

--- src/ElmFive.elm
+++ src/ElmFive.elm
@@ -1,6 +1,11 @@
-module ElmFive exposing (five)
+module ElmFive exposing (five, law)
 
 
 five : Int
 five =
     5
+
+
+law : String
+law =
+    "The Law of Fives states simply that: All things happen in fives, or are divisible by or are multiples of five, or are somehow directly or indirectly appropriate to 5. The Law of Fives is never wrong."
--- tests/ElmFiveTests.elm
+++ tests/ElmFiveTests.elm
@@ -11,4 +11,7 @@ suite =
         [ test "gives five" <|
             \_ ->
                 Expect.equal ElmFive.five 5
+        , test "has a law" <|
+            \_ ->
+                Expect.equal ElmFive.law "The Law of Fives states simply that: All things happen in fives, or are divisible by or are multiples of five, or are somehow directly or indirectly appropriate to 5. The Law of Fives is never wrong."
         ]
