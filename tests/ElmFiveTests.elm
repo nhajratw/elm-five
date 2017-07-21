a6b17279f23746a4ff706bb7c3466a64283adf75
Nayan Hajratwala <nayan@chikli.com>
2017-07-21 18:17:05 -0400

    Finished law of fives

--- tests/ElmFiveTests.elm
+++ tests/ElmFiveTests.elm
@@ -2,7 +2,7 @@ module ElmFiveTests exposing (..)
 
 import Test exposing (Test, describe, test)
 import Expect as Expect
-import ElmFive as ElmFive
+import ElmFive exposing (..)
 
 
 suite : Test
@@ -10,12 +10,16 @@ suite =
     describe "The ElmFive Module"
         [ describe "Basics"
             [ test "gives five" <|
-                \_ ->
-                    Expect.equal ElmFive.five 5
+                \_ -> Expect.equal five 5
             ]
         , describe "The Law of Fives"
             [ test "statement" <|
-                \_ ->
-                    Expect.equal ElmFive.law "The Law of Fives states simply that: All things happen in fives, or are divisible by or are multiples of five, or are somehow directly or indirectly appropriate to 5. The Law of Fives is never wrong."
+                \_ -> Expect.equal law "The Law of Fives states simply that: All things happen in fives, or are divisible by or are multiples of five, or are somehow directly or indirectly appropriate to 5. The Law of Fives is never wrong."
+            , test "addition" <|
+                \_ -> Expect.equal (five + five) 10
+            , test "multiplication" <|
+                \_ -> Expect.equal (five * five) 25
+            , test "division" <|
+                \_ -> Expect.equal (five // five) 1
             ]
         ]
